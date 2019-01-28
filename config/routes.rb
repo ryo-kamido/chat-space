b719de446572005f2743e340e083240eef2061ea
Ryo <Ryo@uetotakashinoMacBook-puro.local>
2019-01-28 20:20:08 +0900

    complete_group
    グループ画面の実装に成功
    今回のコミットでは、編集ボタンの実装
    rootをgroupsに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
+  root 'groups#index'
   resources :users,only: [:update,:edit]
   resources :groups,only: [:new,:edit,:create,:update]
 end
