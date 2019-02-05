05ea8a015a526f0d00d761f62c84362a51229e69
Ryo <Ryo@uetotakashinoMacBook-puro.local>
2019-02-05 18:28:50 +0900

    add_ajaxsystem
    # WHAT
    1. 非同期通信を実現した。
    2. 画像ファイルについては、画像ファイルが送信されたときのみ送信されるように設定した。
    
    # WHY
    チャット機能に必須のため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,41 @@
-$(function(){
-  $('.send').on('submit', function(e){
-    var formData = new FormData(this);
+$(document).on('turbolinks:load',function(){
+  function buildHTML(message){
+  var imagefile = '';
+  if (message.image.url) {
+    imagefile = `<img src="${message.image.url}", class = 'lower-message__image'>`;
+  }
+  var html = `<div class = "message">
+                <div class="high-data">
+                  <div class="high-data__user-name">${message.user_name}</div>
+                  <div class="chat__date">${message.created_at}</div>
+                </div>
+                <div class="low-data">
+                  <p class="low-data__content">${message.content}</p>
+                  ${imagefile}
+                </div>
+              </div>`
+    return html;
+  }
+  $('.new_message').on('submit', function(e){
     e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.message_area').append(html)
+      $('.post-box').val('')
+      $('.message_area').animate({scrollTop: $('.message_area')[0].scrollHeight},'fast');
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
-})
+});
