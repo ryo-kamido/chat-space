e113632f65538eed6f4b411f416ed25ec19c0793
Ryo <Ryo@uetotakashinoMacBook-puro.local>
2019-02-06 14:18:08 +0900

    add_incrementalsearch
    # WHAT
    インクリメンタルサーチを実装して、ビューに反映させた
    # WHY
    機能に必須のため

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,31 @@
+$(function(){
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name=${user.name}>追加</a>
+                </div>`
+    return html;
+  };
+  $("#user-search-field").on("keyup",function(){
+    var input = $("#user-search-field").val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: {keyword: input},
+      dataType: 'json'
+    })
+    .done(function(users){
+      $(".user-search-result").empty();
+      if (users.length !== 0){
+        users.forEach(function(user){
+          var html = appendUser(user);
+          $('#user-search-result').append(html)
+        });
+      }
+    })
+    .fail(function(){
+      alert('ユーザー検索に失敗しました');
+    });
+  });
+});
