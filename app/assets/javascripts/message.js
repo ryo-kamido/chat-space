06090f3ec245f15b0da7862d338177d2f5f8c622
Ryo <Ryo@uetotakashinoMacBook-puro.local>
2019-02-07 17:51:32 +0900

    add_auto_reload
    # WHAT
    Chat-spaceを自動更新するようにした
    # WHY
    チャット機能には必須のため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,10 @@
 $(document).on('turbolinks:load',function(){
   function buildHTML(message){
   var imagefile = message.image.url? `<img src="${message.image.url}", class = 'lower-message__image'>` : "";
-  var html = `<div class = "message">
+  var html = `<div class = "message" data-id=${message.id}>
                 <div class="high-data">
                   <div class="high-data__user-name">${message.user_name}</div>
-                  <div class="chat__date">${message.created_at}</div>
+                  <div class="high-data__date">${message.created_at}</div>
                 </div>
                 <div class="low-data">
                   <p class="low-data__content">${message.content}</p>
@@ -37,5 +37,36 @@ $(document).on('turbolinks:load',function(){
     .always(() => {
       $(".send").removeAttr("disabled");
     });
-  })
+  });
+
+  $(function(){
+    setInterval(update,5000);
+  });
+
+  function update(){
+    console.log(location.href);
+    var message_id = $('.message').last().data('id');
+    console.log(message_id);
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: {
+        message: {id: message_id }
+      },
+      dataType: 'json',
+    })
+    .done(function(messages) {
+      console.log(messages);
+       if (messages.length){
+         insertHTML = "";
+         $.each(messages, function(i, message){
+            insertHTML = buildHTML(message);
+           $('.message_area').append(insertHTML)
+         })
+       }
+     })
+     .fail(function() {
+       alert('自動更新に失敗しました');
+     });
+  }
 });
---
+++ app/views/messages/index.json.jbuilder
@@ -0,0 +1,7 @@
+json.array! @new_messages.each do |message|
+  json.content       message.content
+  json.image         message.image
+  json.id            message.id
+  json.user_name     message.user.name
+  json.created_at    message.created_at.strftime("%Y/%m/%d %H:%M")
+end
